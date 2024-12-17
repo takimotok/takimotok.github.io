b3b0ad12d8244b1e8086c025da4b7e18710f205e
k.takimoto <55965039+takimotok@users.noreply.github.com>
2024-12-17 10:43:29 +0900

    change: add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,67 @@
+import type { Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
+import { fontFamily } from 'tailwindcss/defaultTheme'
+import type { PluginAPI } from 'tailwindcss/types/config'
+
+/** @type {Config} */
+const tailwindConfig: Config = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      lineHeight: {
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
+      },
+      fontFamily: {
+        sans: ['Onest', ...fontFamily.sans],
+      },
+      colors: {
+        primary: colors.pink,
+        gray: colors.gray,
+      },
+      typography: (theme: PluginAPI['theme']) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: theme('colors.primary.600'),
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1, h2': {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+            },
+            h3: {
+              fontWeight: '600',
+            },
+            code: {
+              color: theme('colors.indigo.500'),
+            },
+          },
+        },
+        invert: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: theme('colors.primary.400'),
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1, h2, h3, h4, h5, h6': {
+              color: theme('colors.gray.100'),
+            },
+          },
+        },
+      }),
+    },
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+}
+
+export default tailwindConfig
