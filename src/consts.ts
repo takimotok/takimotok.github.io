49393e95debe161adaee95f037a59fdbe791da34
k.takimoto <55965039+takimotok@users.noreply.github.com>
2024-12-19 16:49:23 +0900

    change: change sentences at Welcome page

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,6 @@
  */
 export const SITE_METADATA = {
   siteUrl: 'https://takimotok.github.io/takimotok.github.io',
+  author: 'Kengo Takimoto',
+  description: "Kengo's resume.\nThis is sample description.",
 }
