2c069b4efe38f65b64fc639296417e2e4a72465b
k.takimoto <55965039+takimotok@users.noreply.github.com>
2024-12-21 18:13:39 +0900

    change: add Footer component

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,8 @@
  */
 export const SITE_METADATA = {
   siteUrl: 'https://takimotok.github.io/takimotok.github.io',
+  siteRepo: 'https://github.com/takimotok/takimotok.github.io',
+  siteTitle: "Kengo's Resume",
   author: 'Kengo Takimoto',
   description: "Kengo's resume.\nThis is sample description.",
 }
