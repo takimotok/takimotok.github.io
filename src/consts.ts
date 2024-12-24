165c3290d3fd425d9092771b54ad61013dc02ae7
k.takimoto <55965039+takimotok@users.noreply.github.com>
2024-12-24 15:48:33 +0900

    tweak: change consts

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,7 @@
  * SITE_METADATA: used across the site
  */
 export const SITE_METADATA = {
-  site: {
-    url: 'https://takimotok.github.io/takimotok.github.io',
-    repo: 'https://github.com/takimotok/takimotok.github.io',
-    title: "Kengo's Resume",
-  },
+  url: 'https://takimotok.github.io/takimotok.github.io',
+  repo: 'https://github.com/takimotok/takimotok.github.io',
+  title: "Kengo's Resume",
 }
